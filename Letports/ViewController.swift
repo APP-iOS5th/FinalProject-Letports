70317d5f028ce196d532dc99b207eb0e6be56b1a
seonggeun an <eventscripts@nate.com>
2024-08-02 14:41:00 +0900

    프로젝트 추가

---
+++ Letports/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Letports
+//
+//  Created by mosi on 8/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LetportsUITests/LetportsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  LetportsUITestsLaunchTests.swift
+//  LetportsUITests
+//
+//  Created by mosi on 8/2/24.
+//
+
+import XCTest
+
+final class LetportsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
