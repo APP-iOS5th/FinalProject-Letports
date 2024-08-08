330461908bcf8c73cf1111bd88544d86f8d3f1e0
Jongchan Yun <jcyun1996@gmail.com>
2024-08-08 14:35:09 +0900

    add selectedImage & tabbar tintcolor

--- Letports/Letports/TabBarController.swift
+++ Letports/Letports/TabBarController.swift
@@ -12,21 +12,36 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupVC()
+        tabBar.tintColor = .lpMain
     }
     
     private func setupVC() {
+        let homeVC = setupNav(for: HomeVC(),
+                              title: "홈",
+                              image: UIImage(systemName: "house"),
+                              selectedImage: UIImage(systemName: "house.fill"))
         
-        let homeVC = setupNav(for: HomeVC(), title: "홈", image: UIImage(systemName: "house"))
-        let gatheringVC = setupNav(for: GatheringVC(), title: "소모임", image: UIImage(systemName: "person.3"))
-        let profileVC = setupNav(for: ProfileVC(), title: "프로필", image: UIImage(systemName: "person"))
+        let gatheringVC = setupNav(for: GatheringVC(),
+                                   title: "소모임",
+                                   image: UIImage(systemName: "person.3"),
+                                   selectedImage: UIImage(systemName: "person.3.fill"))
+        
+        let profileVC = setupNav(for: ProfileVC(),
+                                 title: "프로필",
+                                 image: UIImage(systemName: "person"),
+                                 selectedImage: UIImage(systemName: "person.fill"))
         
         viewControllers = [homeVC, gatheringVC, profileVC]
     }
     
-    private func setupNav(for rootVC: UIViewController, title: String, image: UIImage?) -> UIViewController {
+    private func setupNav(for rootVC: UIViewController,
+                          title: String,
+                          image: UIImage?,
+                          selectedImage: UIImage? = nil) -> UIViewController {
         let navController = UINavigationController(rootViewController: rootVC)
         navController.tabBarItem.title = title
         navController.tabBarItem.image = image
+        navController.tabBarItem.selectedImage = selectedImage
         return navController
     }
 }
